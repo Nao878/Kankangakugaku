bcd5f5729e3e38b4364743b7cd002fce307feff4
Nao878 <yanaoto220@gmail.com>
2025-11-24 09:25:45 +0900

    Added Inferno ending

--- Assets/Script/AnimationController.cs
+++ Assets/Script/AnimationController.cs
@@ -8,6 +8,9 @@ public class AnimationController : MonoBehaviour
     // Inspectorで設定できるフラグ。チェックされているとアニメ終了時に3つ目のエンディングを解禁する
     public bool Kamigata = false;
 
+    // 新しいフラグ: チェックされているとアニメ終了時に4つ目のエンディングを解禁する
+    public bool Infelno = false;
+
     public void OnAnimationEnd()
     {
         // Kamigataフラグが有効ならエンディング3を解禁
@@ -16,6 +19,14 @@ public class AnimationController : MonoBehaviour
             PlayerPrefs.SetInt("Ending_3", 1);
             PlayerPrefs.Save();
             Debug.Log("Unlocked Ending_3 due to Kamigata flag in AnimationController.");
+        }
+
+        // Infelnoフラグが有効ならエンディング4を解禁
+        if (Infelno)
+        {
+            PlayerPrefs.SetInt("Ending_4", 1);
+            PlayerPrefs.Save();
+            Debug.Log("Unlocked Ending_4 due to Infelno flag in AnimationController.");
         }
 
         SceneManager.LoadScene(nextSceneName);
